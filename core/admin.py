5bab9ea14aab1a6510a746501711f5795d91d0dc
s1320228 <s1320228@u-aizu.ac.jp>
2026-08-02 21:37:27 +0900

    Add Available dates to admin page

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,8 @@
 # Register your models here.
 from django.contrib import admin
 
-from .models import Instructor, Reservation
+from .models import Instructor, Reservation, AvailableDate
 
 admin.site.register(Instructor)
 admin.site.register(Reservation)
+admin.site.register(AvailableDate)
