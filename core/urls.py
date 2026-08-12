7b974b2702324fc2c5b1149a9bebd1b572ae27dc
s1320228 <s1320228@u-aizu.ac.jp>
2026-08-12 15:33:59 +0900

    Modified url error

--- core/urls.py
+++ core/urls.py
@@ -109,6 +109,6 @@ urlpatterns = [
     path(
         "reset/done/",
         auth_views.PasswordResetCompleteView.as_view(),
-        name="password_reset_completed"
+        name="password_reset_complete"
     ),
 ]
